93b5f7d1f2197f3b7e866b34702ac517648ad3ec
NadiaNurm <65546559+NadiaNurm@users.noreply.github.com>
2023-03-09 14:27:13 +0300

    Delete TicTacToe directory

--- TicTacToe/qlabelnew.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import pyqtSignal
-
-class QLabelNew(QtWidgets.QLabel):
-    clicked = pyqtSignal()
-    def mousePressEvent(self, ev) -> None:
-        self.clicked.emit()
--- TicTacToe/updated_design.bat
+++
@@ -1,4 +0,0 @@
-python -m PyQt5.uic.pyuic -x design.ui -o design.py
-python -m PyQt5.pyrcc_main Res.qrc -o Res_rc.py
-
-
